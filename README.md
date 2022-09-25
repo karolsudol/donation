f25f3e83ecd178bcf4afb8e0e35972ded5621cdb
elcomendante <karol@finsane.com>
2022-09-25 23:20:18 +0100

    docs

--- README.md
+++ README.md
@@ -1,14 +1,3 @@
-[![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-c66648af7eb3fe8bc4f294546bfd86ef473780cde1dea487d3c4ff354943c9ae.svg)](https://classroom.github.com/online_ide?assignment_repo_id=8635892&assignment_repo_type=AssignmentRepo)
-# Sample Hardhat Project
+# A Donation contract
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
-
-Try running some of the following tasks:
-
-```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat run scripts/deploy.ts
-```
+A contract that allows address that deployed it to receive funds and then withdraw them back to the same address with full visibility of received funds.
--- contracts/Donation.sol
+++ contracts/Donation.sol
@@ -1,32 +1,64 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+/**
+ * @title A Donation contract
+ * @dev A contract that allows address that deployed it to receive funds and then withdraw them back to the same address with full visibility of received funds.
+ */
 contract Donation {
+
+    /** **** Events **** */
+    event Donate(address indexed from, uint256 amount, uint256 ts);
+    event ReleaseFunds(address indexed to, uint256 amount, uint256 ts);
+
+    /** **** State Variables **** */
     address public owner;
     mapping(address => uint256) public amounts;
     address[] public donors;
-
 
     constructor () {
         owner = msg.sender;
     }
 
+    /** **** View Functions ****  */
+
+
+  /**
+   * @dev getDonors returns array of donors addresses
+   *
+   */
+    function getDonors ()external view returns (address[] memory ){
+        return donors;   
+    }
+
+    /** **** State Changing External Functions **** */
+
+  /**
+   * @dev donate accepts funds as a payment and records its sender and amount it received
+   *
+   * Emits a {Donate} event.
+   */
     function donate() external payable {
-        uint256 amount = msg.value;
+        uint256  amount  = msg.value;
 
         amounts[msg.sender] += amount;
         donors.push(msg.sender);
+
+        emit Donate(msg.sender, amount, block.timestamp);
     }
 
+  /**
+   * @dev releaseFunds releases funds to the owner of the contract.
+   *
+   * @param to owners address
+   * @param amount amount denominated in ETH
+   * Emits a {ReleaseFunds} event.
+   */
     function releaseFunds(address to, uint256 amount) external {
         require(msg.sender == owner);
-
         payable(to).transfer(amount);
-        
-    }
 
-    function getDonors ()external view returns (address[] memory ){
-        return donors;   
+        emit ReleaseFunds(owner, amount, block.timestamp); 
     }
 
 }
